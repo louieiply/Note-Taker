a847b0191510d8dce40561d438dd130ab708eb92
louieiply <louieiply@gmail.com>
2022-05-26 00:29:43 +0930

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const api = require("./routes/index");
 const path = require("path");
 
-const PORT = process.env.port || 3002;
+const PORT = process.env.PORT || 3002;
 const app = new express();
 
 app.use(express.json());
